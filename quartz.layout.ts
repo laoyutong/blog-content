fe809614789733b17b03b9287631a65602f84418
laoyutong <1262145400@qq.com>
2026-06-21 22:27:35 +0800

    style: refine quartz theme and line breaks

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -41,6 +41,7 @@ export const defaultContentPageLayout: PageLayout = {
   ],
   right: [
     Component.DesktopOnly(Component.TableOfContents()),
+    Component.DesktopOnly(Component.Graph()),
     Component.Backlinks(),
   ],
 }
